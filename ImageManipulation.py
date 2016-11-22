84cba906534b90cd84a05bd8b396929c410bf6e0
shonihei <shonihei@gmail.com>
2016-11-22 11:49:23 -0500

    Edited Comments

--- ImageManipulation.py
+++ ImageManipulation.py
@@ -1,8 +1,13 @@
+# Written by Sho Nihei
+
 from PIL import Image, ImageDraw
 from random import randint
 
 class CellularAutomaton:
     
+    '''
+        Initialize new cellular automaton object with given properties.
+    '''
     def __init__(self, cD, nC, nR, r, act=["#483C6C", "#433765", "#726A99"], inact=["#FF217C", "#FD207A", "#FFA283"]):
         self.cellDimension = cD
         self.numberOfCols = nC
@@ -19,6 +24,9 @@ class CellularAutomaton:
 
         self.dr = ImageDraw.Draw(self.result)
 
+    '''
+        Convert rulenumber in decimals to rule 
+    '''
     def makeRule(self, ruleNum):
         out = []
         binary = '{0:08b}'.format(ruleNum)
@@ -29,6 +37,10 @@ class CellularAutomaton:
                 out.append(False)
         return out
 
+    '''
+        Randomize the first row so that the rows thereafter can follow the
+        rule provided by self.rule
+    '''
     def randomizeFirstRow(self):
         for x in range(0, self.numberOfCols * self.cellDimension, self.cellDimension):
             randomChoice = randint(0, 1)
@@ -43,6 +55,9 @@ class CellularAutomaton:
             if self.resultPixels[x, 0] == (255, 255, 255):
                 self.dr.rectangle(((x, 0), (x + self.cellDimension-1, self.cellDimension-1)), fill=newColor)
 
+    '''
+        randomize the first row and generate the remaining image
+    '''
     def make(self):
         self.randomizeFirstRow()
         
@@ -66,13 +81,24 @@ class CellularAutomaton:
                 self.checkRules(data, [0, 0, 1], self.rule[6])
                 self.checkRules(data, [0, 0, 0], self.rule[7])
 
+    '''
+        Save the current cellular automaton in the project directory
+    '''
     def save(self, filename):
         self.result.save(filename + ".jpg", format='JPEG', subsampling=0, quality=100)
         return True
-        
+    
+    '''
+        return the tuple of the x,y coordinates of the cell right 
+        above the input
+    '''
     def prevSelf(self, x, y):
         return (x, y - self.cellDimension)
 
+    '''
+        return the tuple of the x,y coordinates of the cell above
+        and to the left of the input
+    '''
     def prevLeft(self, x, y):
         if x == 0:
             x = self.xSize - self.cellDimension
@@ -80,6 +106,10 @@ class CellularAutomaton:
             x = x - self.cellDimension
         return (x, y - self.cellDimension)
 
+    '''
+        return the tuple of the x,y coordinates of the cell above
+        and to the right of the input
+    '''
     def prevRight(self, x, y):
         if x == self.xSize - self.cellDimension:
             x = 0
@@ -87,12 +117,22 @@ class CellularAutomaton:
             x = x + self.cellDimension
         return (x, y - self.cellDimension)
 
+    '''
+        convert colors in rgb notation to hex code
+    '''
     def rgb_to_hex(self, rgb):
         return '#%02x%02x%02x' % rgb
 
+    '''
+        check if a given color is in the list of active colors
+    '''
     def isActive(self, color):
         return self.rgb_to_hex(color) in self.active
 
+    '''
+        draw rectangle according to which state a specific cell at (x, y) needs 
+        to be in
+    '''
     def setState(self, x, y, state):
         ActiveColorIndex = randint(0, len(self.active) - 1)
         InactiveColorIndex = randint(0, len(self.inactive) - 1)
@@ -101,17 +141,12 @@ class CellularAutomaton:
         else:
             self.dr.rectangle(((x, y), (x + self.cellDimension-1, y + self.cellDimension-1)), fill=self.inactive[InactiveColorIndex])
 
+    '''
+        check the three reference states and set the current state accordingly
+    '''
     def checkRules(self, data, rule, ruleOutcome):
         # data = [pLState, pSState, pRState, x, y]
         onOffArray = [data[0], data[1], data[2]]
 
         if onOffArray == rule:
             self.setState(data[3], data[4], ruleOutcome)
-'''
-# Note: 73 is the super cool futuristic one
-filename = input("File Name? : ")
-
-# Settings
-active = ["#483C6C", "#433765", "#726A99"]
-inactive = ["#FF217C", "#FD207A", "#FFA283"]
-'''
